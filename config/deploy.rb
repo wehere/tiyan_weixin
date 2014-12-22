e66beb5c40eb650cbdc032de64331e7ee193dd9e
陈小朋 <beslow@chenxiaopengdeMacBook-Pro.local>
2014-12-22 15:56:04 +0800

    tmp ignore

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,13 +54,13 @@ set :unicorn_config, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "/home/deploy/pids/unicorn.pid"
 namespace :deploy do
 
-  task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && RAILS_ENV=production bundle exec unicorn_rails -c #{unicorn_config} -D -p 3000"
-  end
-
-  task :stop, :roles => :app, :except => { :no_release => true } do
-    run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
-  end
+  # task :start, :roles => :app, :except => { :no_release => true } do
+  #   run "cd #{current_path} && RAILS_ENV=production bundle exec unicorn_rails -c #{unicorn_config} -D -p 3000"
+  # end
+  #
+  # task :stop, :roles => :app, :except => { :no_release => true } do
+  #   run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  # end
 
   # task :restart, :roles => :app, :except => { :no_release => true } do
   #   # 用USR2信号来实现无缝部署重启
