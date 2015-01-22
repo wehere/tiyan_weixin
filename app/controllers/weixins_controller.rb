9bd052f02a2719da6b22b82f5858d2d47071cd46
陈小朋 <beslow@chenxiaopengdeMacBook-Pro.local>
2015-01-22 10:58:16 +0800

    ....

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -1,6 +1,6 @@
 class WeixinsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter :check_weixin_legality
+  # before_filter :check_weixin_legality
   def show
     render :text => params[:echostr]
   end
